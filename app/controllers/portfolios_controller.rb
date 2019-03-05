ac98da2d1a5c42a87db4cddde6ffb5ffc33e0071
Muhammad Tauseef <m.tauseef.rc@gmail.com>
2019-03-05 23:55:27 +0500

    implemented show functionality for portfolio module

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -25,7 +25,7 @@ class PortfoliosController < ApplicationController
   end
 
   def update
-        @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item = Portfolio.find(params[:id])
 
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
@@ -36,4 +36,9 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
 end
