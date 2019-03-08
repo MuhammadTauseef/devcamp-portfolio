672e7b79e612b19868cc9447799452a48d964cba
Muhammad Tauseef <m.tauseef.rc@gmail.com>
2019-03-08 07:35:08 +0500

    implemented concerns for managing image data

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,5 +1,10 @@
 class Skill < ApplicationRecord
+  include placeholder
+  validates_presence_of :title, :percent_utalized
 
-validates_presence_of :title, :percent_utalized
+  after_initialize :set_defaults
 
+  def set_defaults
+    self.badge ||=  placeholder.image_generator(height: '250', width: '250')
+  end
 end
